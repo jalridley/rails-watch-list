3ffa2d73a1d1a812ce0e2c192b854bcc4d214b17
Jalynn Ridley <jallywally@gmail.com>
2021-05-21 16:17:23 +0200

    all but destroy completed without styling

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,5 +7,25 @@ class ListsController < ApplicationController
     # find the index of chosen list
     @list = List.find(params[:id])
   end
+
+  def new
+    @list = List.new
+  end
   
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+
 end
